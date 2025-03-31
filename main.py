cafb7eaaa56d914778133e554b06b07ab8e11bb8
bman-prog <bmankovski2028@communityschoolnaples.org>
2025-03-31 11:39:33 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+@namespace
+class SpriteKind:
+    left = SpriteKind.create()
+    right = SpriteKind.create()
+
+def on_overlap(sprite, otherSprite):
+    ball.set_velocity(100, 100)
+sprites.on_overlap(SpriteKind.projectile, SpriteKind.left, on_overlap)
+
+def on_overlap2(sprite, otherSprite):
+    ball.set_velocity(randint(0, -100), randint(0, -100),)
+sprites.on_overlap(SpriteKind.projectile, SpriteKind.right, on_overlap2)
+
 plate_left = sprites.create(img("""
     . . 8 8 8 . .
     . . 8 8 8 . .
@@ -24,8 +37,7 @@ plate_left = sprites.create(img("""
     . . 8 8 8 . .
     . . 8 8 8 . .
     . . 8 8 8 . .
-"""), SpriteKind.player)
-
+"""), SpriteKind.left)
 
 plate_right = sprites.create(img("""
     . . 8 8 8 . .
@@ -53,13 +65,37 @@ plate_right = sprites.create(img("""
     . . 8 8 8 . .
     . . 8 8 8 . .
     . . 8 8 8 . .
-"""), SpriteKind.player)
+"""), SpriteKind.right)
 
 plate_left.set_position(10, 60)
 plate_right.set_position(150, 60)
 plate_left.set_stay_in_screen(True)
 plate_right.set_stay_in_screen(True)
 
+ball = sprites.create(img("""
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+"""), SpriteKind.projectile)
+ball.set_scale(.3, ScaleAnchor.MIDDLE)
+ball.set_velocity(100, 100)
+ball.set_stay_in_screen(True)
+ball.set_bounce_on_wall(True)
 
 controller.move_sprite(plate_right, 0, 195)
 
--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+namespace SpriteKind {
+    export const left = SpriteKind.create()
+    export const right = SpriteKind.create()
+}
+
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.left, function on_overlap(sprite: Sprite, otherSprite: Sprite) {
+    ball.setVelocity(100, 100)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.right, function on_overlap2(sprite: Sprite, otherSprite: Sprite) {
+    ball.setVelocity(randint(0, -100), randint(0, -100))
+})
 let plate_left = sprites.create(img`
     . . 8 8 8 . .
     . . 8 8 8 . .
@@ -24,7 +35,7 @@ let plate_left = sprites.create(img`
     . . 8 8 8 . .
     . . 8 8 8 . .
     . . 8 8 8 . .
-`, SpriteKind.Player)
+`, SpriteKind.left)
 let plate_right = sprites.create(img`
     . . 8 8 8 . .
     . . 8 8 8 . .
@@ -51,11 +62,35 @@ let plate_right = sprites.create(img`
     . . 8 8 8 . .
     . . 8 8 8 . .
     . . 8 8 8 . .
-`, SpriteKind.Player)
+`, SpriteKind.right)
 plate_left.setPosition(10, 60)
 plate_right.setPosition(150, 60)
 plate_left.setStayInScreen(true)
 plate_right.setStayInScreen(true)
+let ball = sprites.create(img`
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+    11111111111111111111
+`, SpriteKind.Projectile)
+ball.setScale(.3, ScaleAnchor.Middle)
+ball.setVelocity(100, 100)
+ball.setStayInScreen(true)
+ball.setBounceOnWall(true)
 controller.moveSprite(plate_right, 0, 195)
 game.onUpdate(function move_left_paddle() {
     if (controller.B.isPressed()) {
